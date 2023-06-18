c711f337abe8a82175b3542cf136119c15250450
barinder <barindersingh1997@gmail.com>
2023-06-18 15:41:34 +0530

    all css and links changes

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url :"/#/crypto",
+        cName:"nav-s",
+        icon:"fa-solid fa-house"
+    },
+    
+    {
+        title:'Exchanges',
+        url : "/crypto/#/exchanges",
+        cName:"nav-s",
+        icon:"fa-solid fa-arrow-trend-up"
+    },
+    
+    {
+        title:'Coins',
+        url :"/crypto/#/coins",
+        cName:"nav-s",
+        icon:"fa-brands fa-bitcoin"
+    }
+]
